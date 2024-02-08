da93bd068152823d927d19d98f6440911a326daa
dodonk4 <50113883+dodonk4@users.noreply.github.com>
2024-02-07 23:37:33 -0300

    7/2/2024 Images problem

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ connectDB();
 
 const stripe = new Stripe("sk_test_51OL4E7GEU6GUNtdOv49pwzQH9GIm5A0g8CgPDVSEFSDNADq8mp632AsUJ1HiHhvZlVbjAvyEU6td3u98kNuXiKiq00SJYm8Dhf"); 
 
-app.use(express.static('public'));
+app.use(express.static('static'));
 app.use(router);
 app.use(errorHandlerer);
 
