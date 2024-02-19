6b29cf4bbb8b328f160ae77e7b58895437606ff3
dodonk4 <50113883+dodonk4@users.noreply.github.com>
2024-02-18 23:00:30 -0300

    CORS 18/2/2024

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import connectDB from './config/index.config.js';
 import jwt from 'jsonwebtoken';
 import errorHandlerer from './middlewares/errorHandlerer.js';
 import Stripe from 'stripe'
+import cors from 'cors'
 
 dotenv.config();
 
@@ -15,9 +16,11 @@ connectDB();
 
 const stripe = new Stripe("sk_test_51OL4E7GEU6GUNtdOv49pwzQH9GIm5A0g8CgPDVSEFSDNADq8mp632AsUJ1HiHhvZlVbjAvyEU6td3u98kNuXiKiq00SJYm8Dhf"); 
 
+app.use(cors());
 app.use(express.static('static'));
 app.use(router);
 app.use(errorHandlerer);
+
 
 
 
